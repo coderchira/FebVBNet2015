44c547f9a2201df5606f0bffd6b37a6fba950144
coderchira <ruchira.aggarwal@gmail.com>
2015-02-26 11:37:16 +0530

    LogicDemo

---
+++ FebVBSessions/LogicDemo.vb
@@ -0,0 +1,129 @@
+﻿Module LogicDemo
+    'Logic
+    '   Functions
+    '       Return a value
+    '   Subroutines
+    '       Do not a return value
+
+    'Lifecycle
+    '   Declare
+    '   Implement it (Body of the function/Subroutine)
+    '   Call it from another logic block
+    '   Test
+
+    'Function Signature
+    '   Name of the function
+    '   Security
+    '   No. of parameters
+    '   Sequence of parameters
+    '   Type of parameters
+    '   Return Type
+
+    Sub DoThis()
+        'Body of the subroutine
+    End Sub
+
+    Sub DoThisAsWell()
+        'Call a subroutine
+        DoThis()
+
+        'Call a function
+        Dim Flag As Boolean
+        Flag = Calculation(25, 58.0, "Ruchira")
+        'Calculation("Ruchira", 12)
+
+        Dim num As Integer
+        num = Calculation(True)
+
+    End Sub
+
+    'Function overloading
+    'Polymorphism
+    Function Calculation(age As Integer, weight As Single, name As String) As Boolean
+        Console.WriteLine("We are inside first implementation of calculation function")
+        Console.ReadLine()
+
+        'Conditional Statements
+        'If Statements
+        If age > 25 Then
+            'Body of the if block
+            Console.WriteLine("Hurray, the age is as expected")
+        ElseIf age < 25 And weight >= 55 Then
+            Console.WriteLine("Age is less than expected")
+        Else
+            Console.WriteLine("All the above conditions are false")
+        End If
+        Console.ReadLine()
+
+        'Select case statements
+        Select Case age
+            Case 25
+                'Some implementation
+            Case 26
+                'Do this
+            Case 40, 45, 47
+                'Implement this piece of code
+            Case Else
+                'Do this if above is false
+        End Select
+
+        'Nested if statements
+        If name = "Ruchira" Then
+            'Do this
+            If age >= 25 Then
+            ElseIf age = 40 Then
+            End If
+        End If
+
+        'Looping Statements
+        'For Loop
+        For index As Integer = 1 To 10 Step 2
+            'Body of the loop
+            Console.WriteLine("Students in the class are " & index)
+        Next
+        Console.ReadLine()
+
+        For counter = 10 To 1 Step -1
+            Console.WriteLine("Now the numbers are: " & counter)
+        Next
+        Console.ReadLine()
+
+        'While Loop
+        'Dim index1 As Integer = 1
+        'While index1 >= 1 And age < 25
+        '    'Do this
+        '    Console.WriteLine("Age is " & age)
+        '    index1 = index1 + 1
+        '    age = age - 1
+        'End While
+        'Console.ReadLine()
+
+        'Do While loops
+        Do
+            Console.WriteLine("This will be run atleast once, sinc the condition is checked at the end")
+        Loop While age < 25
+        Console.ReadLine()
+
+        Return True
+    End Function
+
+    Function Calculation(flag As Boolean) As Integer
+
+        Return 0
+    End Function
+
+    Function Calculation(text As String) As String
+        Return "This is another way"
+    End Function
+
+    Function Calculation() As Single
+
+        Return 10.12
+    End Function
+
+    Function MyFunc() As Integer
+
+        Return 0
+    End Function
+
+End Module
--- FebVBSessions/Module1.vb
+++ FebVBSessions/Module1.vb
@@ -1,159 +1,161 @@
 ﻿Module Module1
     Sub Main()
-        'What is a Program?
-        '   Data <---> Logic (Bidirectional relationship)
-        '   Data
-        '       Variables -- Change their value
-        '          1. Native Variables (Stored on Stack)
-        '               They are fixed in size
-        '               Faster access
-        '               Need not be explicitly disposed
-        '          2. Reference Variables/Object variables (Stored on heap)
-        '               Dynamic in size
-        '               Slower
-        '               Need to be explicitly disposed       
-        '       Constants -- do not change their value
-        '   Logic
+        LogicDemo.DoThisAsWell()
 
-        'Lifecycle of a variable
-        '   Declare
-        '   Assign/ Use
-        '   Dispose
+        ''What is a Program?
+        ''   Data <---> Logic (Bidirectional relationship)
+        ''   Data
+        ''       Variables -- Change their value
+        ''          1. Native Variables (Stored on Stack)
+        ''               They are fixed in size
+        ''               Faster access
+        ''               Need not be explicitly disposed
+        ''          2. Reference Variables/Object variables (Stored on heap)
+        ''               Dynamic in size
+        ''               Slower
+        ''               Need to be explicitly disposed       
+        ''       Constants -- do not change their value
+        ''   Logic
 
-        'Declaration
-        Dim Flag As Boolean '2 bytes
-        Dim Num As Integer '4 bytes
-        Dim Precision As Single '4 bytes
-        Dim PrecDbl As Double  '8 bytes
-        Dim Accuracy As Decimal '16 bytes
-        Dim Name As String 'Size depends on the value of string variable
+        ''Lifecycle of a variable
+        ''   Declare
+        ''   Assign/ Use
+        ''   Dispose
 
-        'Assignment
-        Flag = True ' Default = 0 or false
-        Num = 10
-        Precision = 0.1234567
-        PrecDbl = 0.123456789101112
-        Accuracy = 0.12345678910111213
-        Name = "Ruchira"
+        ''Declaration
+        'Dim Flag As Boolean '2 bytes
+        'Dim Num As Integer '4 bytes
+        'Dim Precision As Single '4 bytes
+        'Dim PrecDbl As Double  '8 bytes
+        'Dim Accuracy As Decimal '16 bytes
+        'Dim Name As String 'Size depends on the value of string variable
 
-        'Printing on the console window
-        Console.WriteLine("The vallue of Flag is {0}", Flag)
-        Console.WriteLine("The value of Num is {0}", Num)
-        Console.WriteLine("The value of Precision is {0}", Precision)
-        Console.WriteLine("The value of PrecDbl is {0}", PrecDbl)
-        Console.WriteLine("The value of Accuracy is (0)", Accuracy)
-        Console.WriteLine("The value of Name is {0}", Name)
-        Console.ReadLine()
+        ''Assignment
+        'Flag = True ' Default = 0 or false
+        'Num = 10
+        'Precision = 0.1234567
+        'PrecDbl = 0.123456789101112
+        'Accuracy = 0.12345678910111213
+        'Name = "Ruchira"
 
-        'Integer Operations
-        Dim LeftInt As Integer
-        Dim RightInt As Integer
-        Dim SumOfIntegers As Integer
-        Dim DiffOfIntegers As Integer
-        Dim ProdOfIntegers As Integer
-        Dim DivOfIntegers As Integer
-        Dim Quotient As Integer
-        Dim Remainder As Integer
-
-        LeftInt = 10
-        RightInt = 20
-        SumOfIntegers = LeftInt + RightInt
-        DiffOfIntegers = RightInt - LeftInt
-        ProdOfIntegers = LeftInt * RightInt
-        DivOfIntegers = RightInt / LeftInt
-        Quotient = Math.DivRem(RightInt, LeftInt, Remainder)
-
-        'Printing on the console window
-        Console.WriteLine("The value of LeftInt is {0}", LeftInt)
-        Console.WriteLine("The value of RightInt is {0}", RightInt)
-        Console.WriteLine("The Sum of numbers is {0}", SumOfIntegers)
-        Console.WriteLine("The Difference of numbers is {0}", DiffOfIntegers)
-        Console.WriteLine("The Product of numbers is {0}", ProdOfIntegers)
-        Console.WriteLine("The Division of numbers is {0}", DivOfIntegers)
-        Console.WriteLine("The value of Remainder is {0} and Quotient is {1}", Remainder, Quotient)
-        Console.ReadLine()
-
-        'String Operations
-        Dim Message1 As String
-        Dim Message2 As String
-        Dim ResultMessage As String
-        Dim Position1 As Integer
-        Dim Position2 As Integer
-        Dim LeftString As String
-        Dim RightString As String
-
-        Message1 = "This is Day 1 code of VB session"
-        Message2 = " and we are confident of learning VB"
-
-        'Concatenation of Strings
-        'ResultMessage = Message1 + Message2 -- Use Sparingly as will falter when trying to concatenate numerals
-        ResultMessage = Message1 & Message2
-        Console.WriteLine("ResultMessage : " & ResultMessage)
-        Console.ReadLine()
-
-        Position1 = InStr(ResultMessage, "vb".ToUpper())
-        Position2 = InStr(Position1 + 1, ResultMessage, "VB")
-        Console.WriteLine("First occurance of VB is at {0} and second occurance at {1}", Position1, Position2)
-        Console.ReadLine()
-
-        LeftString = Left(ResultMessage, 14)
-        RightString = Right(ResultMessage, 25)
-        Console.WriteLine("Left Portion of the string is : " & LeftString)
-        Console.WriteLine("Right Portion of the string is : " & RightString)
-        Console.ReadLine()
-
-        'Conversion operations
-        Dim NewNumber As Integer
-        Dim NumberAsString As String
-        NewNumber = 10
-        NumberAsString = NewNumber.ToString()
-        NumberAsString = CStr(NewNumber)
-        Console.WriteLine("Converted number is " & NumberAsString)
-        Console.ReadLine()
-
-        Dim NewString As String
-        Dim StringAsNumber As Integer
-        NewString = "18"
-        StringAsNumber = CInt(NewString)
-        Console.WriteLine("Converted string is " & StringAsNumber)
-        Console.ReadLine()
-
-        Dim NewString2 As String
-        Dim StringAsDouble As Double
-        NewString2 = "1.79"
-        StringAsDouble = CDbl(NewString2)
-        Console.WriteLine("Converted string2 is " & StringAsDouble)
-        Console.ReadLine()
-
-        'Reference type Variables
-        'Declaration
-        Dim Collection1 As Collection
-
-        ' New Keyword is necessary for instantiating an object variable
-        Collection1 = New Collection
-
-        'Adding items of any data type to the collection
-        Collection1.Add("Ruchira")
-        Collection1.Add("Venkat")
-        Collection1.Add("Vijay")
-        Collection1.Add(1)
-        Collection1.Add(1.98)
-        Collection1.Add(2)
-
-        'Retrieving an item from a collection
-        'Collections are 1-based
-        Console.WriteLine("Items in collection are {0}, {1}, {2}, {3}, {4}, {5}", Collection1.Item(1), Collection1.Item(2), Collection1.Item(3), Collection1.Item(4), Collection1.Item(5), Collection1.Item(6))
-        Console.ReadLine()
-
-        'Removal of an item from a collection
-        Collection1.Remove(5)
-        Console.WriteLine("New Items in the collection are {0}, {1}, {2}, {3}, {4}", Collection1.Item(1), Collection1.Item(2), Collection1.Item(3), Collection1.Item(4), Collection1.Item(5))
-        Console.ReadLine()
-
-        'Dispose the collection
-        Collection1 = Nothing
-        'Retrieval of an item will now throw a null pointer exception
-        'Console.WriteLine("Items in collection are {0}, {1}, {2}, {3}, {4}", Collection1.Item(1), Collection1.Item(2), Collection1.Item(3), Collection1.Item(4), Collection1.Item(5))
+        ''Printing on the console window
+        'Console.WriteLine("The vallue of Flag is {0}", Flag)
+        'Console.WriteLine("The value of Num is {0}", Num)
+        'Console.WriteLine("The value of Precision is {0}", Precision)
+        'Console.WriteLine("The value of PrecDbl is {0}", PrecDbl)
+        'Console.WriteLine("The value of Accuracy is (0)", Accuracy)
+        'Console.WriteLine("The value of Name is {0}", Name)
         'Console.ReadLine()
+
+        ''Integer Operations
+        'Dim LeftInt As Integer
+        'Dim RightInt As Integer
+        'Dim SumOfIntegers As Integer
+        'Dim DiffOfIntegers As Integer
+        'Dim ProdOfIntegers As Integer
+        'Dim DivOfIntegers As Integer
+        'Dim Quotient As Integer
+        'Dim Remainder As Integer
+
+        'LeftInt = 10
+        'RightInt = 20
+        'SumOfIntegers = LeftInt + RightInt
+        'DiffOfIntegers = RightInt - LeftInt
+        'ProdOfIntegers = LeftInt * RightInt
+        'DivOfIntegers = RightInt / LeftInt
+        'Quotient = Math.DivRem(RightInt, LeftInt, Remainder)
+
+        ''Printing on the console window
+        'Console.WriteLine("The value of LeftInt is {0}", LeftInt)
+        'Console.WriteLine("The value of RightInt is {0}", RightInt)
+        'Console.WriteLine("The Sum of numbers is {0}", SumOfIntegers)
+        'Console.WriteLine("The Difference of numbers is {0}", DiffOfIntegers)
+        'Console.WriteLine("The Product of numbers is {0}", ProdOfIntegers)
+        'Console.WriteLine("The Division of numbers is {0}", DivOfIntegers)
+        'Console.WriteLine("The value of Remainder is {0} and Quotient is {1}", Remainder, Quotient)
+        'Console.ReadLine()
+
+        ''String Operations
+        'Dim Message1 As String
+        'Dim Message2 As String
+        'Dim ResultMessage As String
+        'Dim Position1 As Integer
+        'Dim Position2 As Integer
+        'Dim LeftString As String
+        'Dim RightString As String
+
+        'Message1 = "This is Day 1 code of VB session"
+        'Message2 = " and we are confident of learning VB"
+
+        ''Concatenation of Strings
+        ''ResultMessage = Message1 + Message2 -- Use Sparingly as will falter when trying to concatenate numerals
+        'ResultMessage = Message1 & Message2
+        'Console.WriteLine("ResultMessage : " & ResultMessage)
+        'Console.ReadLine()
+
+        'Position1 = InStr(ResultMessage, "vb".ToUpper())
+        'Position2 = InStr(Position1 + 1, ResultMessage, "VB")
+        'Console.WriteLine("First occurance of VB is at {0} and second occurance at {1}", Position1, Position2)
+        'Console.ReadLine()
+
+        'LeftString = Left(ResultMessage, 14)
+        'RightString = Right(ResultMessage, 25)
+        'Console.WriteLine("Left Portion of the string is : " & LeftString)
+        'Console.WriteLine("Right Portion of the string is : " & RightString)
+        'Console.ReadLine()
+
+        ''Conversion operations
+        'Dim NewNumber As Integer
+        'Dim NumberAsString As String
+        'NewNumber = 10
+        'NumberAsString = NewNumber.ToString()
+        'NumberAsString = CStr(NewNumber)
+        'Console.WriteLine("Converted number is " & NumberAsString)
+        'Console.ReadLine()
+
+        'Dim NewString As String
+        'Dim StringAsNumber As Integer
+        'NewString = "18"
+        'StringAsNumber = CInt(NewString)
+        'Console.WriteLine("Converted string is " & StringAsNumber)
+        'Console.ReadLine()
+
+        'Dim NewString2 As String
+        'Dim StringAsDouble As Double
+        'NewString2 = "1.79"
+        'StringAsDouble = CDbl(NewString2)
+        'Console.WriteLine("Converted string2 is " & StringAsDouble)
+        'Console.ReadLine()
+
+        ''Reference type Variables
+        ''Declaration
+        'Dim Collection1 As Collection
+
+        '' New Keyword is necessary for instantiating an object variable
+        'Collection1 = New Collection
+
+        ''Adding items of any data type to the collection
+        'Collection1.Add("Ruchira")
+        'Collection1.Add("Venkat")
+        'Collection1.Add("Vijay")
+        'Collection1.Add(1)
+        'Collection1.Add(1.98)
+        'Collection1.Add(2)
+
+        ''Retrieving an item from a collection
+        ''Collections are 1-based
+        'Console.WriteLine("Items in collection are {0}, {1}, {2}, {3}, {4}, {5}", Collection1.Item(1), Collection1.Item(2), Collection1.Item(3), Collection1.Item(4), Collection1.Item(5), Collection1.Item(6))
+        'Console.ReadLine()
+
+        ''Removal of an item from a collection
+        'Collection1.Remove(5)
+        'Console.WriteLine("New Items in the collection are {0}, {1}, {2}, {3}, {4}", Collection1.Item(1), Collection1.Item(2), Collection1.Item(3), Collection1.Item(4), Collection1.Item(5))
+        'Console.ReadLine()
+
+        ''Dispose the collection
+        'Collection1 = Nothing
+        ''Retrieval of an item will now throw a null pointer exception
+        ''Console.WriteLine("Items in collection are {0}, {1}, {2}, {3}, {4}", Collection1.Item(1), Collection1.Item(2), Collection1.Item(3), Collection1.Item(4), Collection1.Item(5))
+        ''Console.ReadLine()
     End Sub
 End Module
