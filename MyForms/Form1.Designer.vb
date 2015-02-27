d5074f2c959aaf3f28dcf469347bba673e5e8352
coderchira <ruchira.aggarwal@gmail.com>
2015-02-27 12:30:26 +0530

    Forms Demo

---
+++ MyForms/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.txtBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(100, 115)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'txtBox
+        '
+        Me.txtBox.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBox.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.txtBox.Location = New System.Drawing.Point(62, 63)
+        Me.txtBox.Name = "txtBox"
+        Me.txtBox.Size = New System.Drawing.Size(158, 20)
+        Me.txtBox.TabIndex = 1
+        Me.txtBox.Text = "Enter text"
+        '
+        'frm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtBox)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "frm"
+        Me.Text = "MyForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents txtBox As System.Windows.Forms.TextBox
+
+End Class
---
+++ MyForms/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class frm
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        btnSubmit.Enabled = True
+        MsgBox("Form is clicked")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnSubmit.Enabled = False
+    End Sub
+
+    Private Sub txtBox_TextChanged(sender As Object, e As EventArgs) Handles txtBox.TextChanged
+        txtBox.ForeColor = Color.Red
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        MsgBox("Button is clicked")
+    End Sub
+End Class
